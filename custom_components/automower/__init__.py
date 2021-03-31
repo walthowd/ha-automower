174596d0c53f15ae26e43073df802c3762c376b4
Matthias Freund <matthias88freund@gmail.com>
2021-03-31 19:54:28 +0200

    add status wait power up
    Co-authored-by: MZorzy <mzorzy@gmail.com>

--- custom_components/automower/__init__.py
+++ custom_components/automower/__init__.py
@@ -47,6 +47,7 @@ STATUS_PAUSED =                 'PAUSED'
 STATUS_EXECUTING_PARK =         'EXECUTING_PARK'
 STATUS_EXECUTING_START =        'EXECUTING_START'
 STATUS_EXECUTING_STOP =         'EXECUTING_STOP'
+STATUS_WAIT_POWER_UP =          'WAIT_POWER_UP'
 STATUS_OFF_HATCH_OPEN =         'OFF_HATCH_OPEN'
 STATUS_OFF_HATCH_CLOSED =       'OFF_HATCH_CLOSED_DISABLED'
 STATUS_OFF_DISABLED =           'OFF_DISABLED'
@@ -65,6 +66,7 @@ STATUSES = {
     STATUS_EXECUTING_START:         { 'icon': 'mdi:dots-horizontal','message': 'Starting...' },
     STATUS_EXECUTING_STOP:          { 'icon': 'mdi:dots-horizontal','message': 'Stopping...' },
     STATUS_EXECUTING_PARK:          { 'icon': 'mdi:dots-horizontal','message': 'Preparing to park...' },
+    STATUS_WAIT_POWER_UP:           { 'icon': 'mdi:dots-horizontal','message': 'Powering up...' },
     STATUS_OFF_HATCH_OPEN:          { 'icon': 'mdi:alert',          'message': 'Hatch opened' },
     STATUS_OFF_HATCH_CLOSED:        { 'icon': 'mdi:pause',          'message': 'Stopped but not on base' },
     STATUS_OFF_DISABLED:            { 'icon': 'mdi:close-circle-outline', 'message': 'Off'}
